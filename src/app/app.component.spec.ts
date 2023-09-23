5930614f45d38521be278ba5c5e7581e85245d6a
Hakim Saoudi <saoudihakim@gmail.com>
2023-09-23 23:30:14 +0200

    rename project as original

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'new-frontend'`, () => {
+  it(`should have as title 'utoronto'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('new-frontend');
+    expect(app.title).toEqual('utoronto');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('new-frontend app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('utoronto app is running!');
   });
 });
